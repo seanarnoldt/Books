8898d00a982372007bb83ecce83d440da2afda0f
Sean Arnoldt <sean.arnoldt6@gmail.com>
2024-09-12 17:09:21 -0500

    add error window

--- gui.py
+++ gui.py
@@ -11,3 +11,15 @@ def test():
             break
 
     window.close()
+
+def error(errorMsg):
+    layout = [[sg.Text(text_color="red", text=errorMsg), [sg.Button("OK")]]]
+    window = sg.Window(title="ERROR", layout=layout)
+
+    while True:
+        event, values = window.read()
+        if event == "OK" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+    
