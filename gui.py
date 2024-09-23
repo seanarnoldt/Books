1bf138e7bee6d027a4819dced60e267cd0b68126
Sean Victor Arnoldt <arnoldts@msu.edu>
2024-09-23 13:38:43 -0500

    add book saving and removal

--- gui.py
+++ gui.py
@@ -4,18 +4,6 @@ import backlog
 import datetime
 import math
 
-def test():
-    layout = [[sg.Text("Welcome to Reading Tracker"), [sg.Button("View Books")]]]
-
-    window = sg.Window("Book Tracker", layout)
-
-    while True:
-        event, values = window.read()
-        if event == "View Books" or event == sg.WIN_CLOSED:
-            break
-
-    window.close()
-
 def error(errorMsg):
     layout = [[sg.Text(text_color="red", text=errorMsg), [sg.Button("OK")]]]
     window = sg.Window(title="ERROR", layout=layout)
@@ -37,21 +25,41 @@ def menu(bl):
             break
 
         if event == "Edit Backlog":
-            editBooks(bl.backlog)
+            editBooks(bl)
 
         if event == "Status":
-            status(bl.backlog)
+            status(bl)
 
         if event == "View Books":
-            displayBooks(bl.backlog)
+            displayBooks(bl)
+
+        if event == "Add Book":
+            addBook(bl)
 
     window.close()
 
+def addBook(bl):
+    layout = [[sg.Text("Title:"), sg.In()], 
+                      [sg.Text("Length:"), sg.In()], 
+                      [sg.Checkbox("Current book", default=False)],
+                      [sg.Text("Current page:"), sg.In(0)], 
+                      [sg.Button("Save")]]
+    window = sg.Window(title="Add a New Book", layout=layout, element_padding=[10,10])
 
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        if event == "Save":
+            newBook = book.Book(values[0], values[1], values[2], values[3])
+            bl.addBook(newBook)
+            bl.saveBacklog()
+            break
+    window.close()
 
 def displayBooks(bl):
     layout = []
-    for book in bl:
+    for book in bl.backlog:
         if book.current:
             layout.append([sg.Text(f"{book.title} {book.length} pages. Currently reading.")])
         else:
@@ -74,14 +82,15 @@ def status(bl):
     current_date = datetime.datetime.now()
     end_of_year = datetime.datetime(current_date.year, 12, 31)
     daysLeft = (end_of_year - current_date).days + 2
-    for book in bl:
+    for book in bl.backlog:
         totalLen += (int(book.length) - int(book.currentPage))
     pagesPerDay = math.ceil(totalLen / daysLeft)
     currentBooks = []
-    for book in bl:
+    for book in bl.backlog:
         if book.current == True:
             currentBooks.append(book)
-    viewStatusLayout = [[sg.Text(f"To complete all of the books in your backlog by the end of the year you need to read {pagesPerDay} pages per day.")]]
+    viewStatusLayout = [[sg.Text(f"There are {daysLeft} days left in the year. You have {totalLen} pages left to read.")]]
+    viewStatusLayout.append([[sg.Text(f"To complete all of the books in your backlog by the end of the year you need to read {pagesPerDay} pages per day.")]])
     if len(currentBooks) > 0:
         for book in currentBooks:
             remaining = int(book.length) - int(book.currentPage)
@@ -105,7 +114,7 @@ def status(bl):
 
 def editBooks(bl):
     titles = []
-    for bk in bl:
+    for bk in bl.backlog:
         titles.append(bk.title)
 
     viewBookLayout = [[sg.Text("Select a book to edit")],
@@ -121,27 +130,31 @@ def editBooks(bl):
 
         if event == "Confirm":
             selectedBook = values["-LISTBOX-"]
-            for bk in bl:
+            for bk in bl.backlog:
                 if bk.title == selectedBook[0]:
-                    editBook(bk)
+                    editedVals = editBook(bl, bk)
+                    if editedVals is not None:
+                        bk.setAll(editedVals)
                     break
+            bl.saveBacklog()
             continue
-
     window.close()
 
-def editBook(book):
+def editBook(bl, book):
     editBookLayout = [[sg.Text("Title:"), sg.In(book.title)], 
                       [sg.Text("Length:"), sg.In(book.length)], 
                       [sg.Checkbox("Current book", default=book.current)],
                       [sg.Text("Current page:"), sg.In(book.currentPage)], 
-                      [sg.Button("Save")]]
+                      [sg.Button("Save"), sg.Button("Complete/Remove")]]
     window = sg.Window(title = f"Edit {book.title}", layout=editBookLayout)
-
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == "Save":
-            print(values)
-
+            window.close()
+            return(values)
+        if event == "Complete/Remove":
+            bl.removeBook(book)
+            break
     window.close()
